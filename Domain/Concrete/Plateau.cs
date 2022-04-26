e169358cf69044df7dc621268c8e5ff621e7e424
murat-guzel <murat-guzel@outlook.com.tr>
2022-04-26 22:12:47 +0300

    Mars Rover project add all files

---
+++ Domain/Concrete/Plateau.cs
@@ -0,0 +1,16 @@
+﻿using Mars_Rover.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Rover.Domain.Concrete
+{
+    public class Plateau : IPlateau
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+    }
+}
